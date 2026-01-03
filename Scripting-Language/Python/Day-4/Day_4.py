9f3d2eb1a99e26615291a8bf57c353c65d14fa51
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-01-03 08:17:49 +0530

    Day-4 Python tuple Data Type

---
+++ Scripting-Language/Python/Day-4/Day_4.py
@@ -0,0 +1,15 @@
+"Tuple DataType"
+
+l = ()
+m = tuple()
+n = 1, 2, 3, 4, 5
+o = 'a',
+t = (10,'python',3.2,True,2+3j,[10,20,30]) #tuple with different data types
+
+print(type(l))  #tuple
+print(type(m))  #tuple
+print(type(n))  #tuple
+print(type(o))  #tuple
+print(type(t))  #tuple
+print(t[1:4])  #slicing
+print(t[-1])   #accessing last element
