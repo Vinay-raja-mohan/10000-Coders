c679fdb25194a76162c08e8d60f449f00871213d
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-01-07 07:53:06 +0530

    Day-7 Python Logical Operators

--- Scripting-Language/Python/Day-7/Day_7.py
+++ Scripting-Language/Python/Day-7/Day_7.py
@@ -4,3 +4,23 @@ b = 'ba'
 print(a == b)        # False
 print("---------------------------------------------------------------------")
 
+"Logical Operators"
+
+# and
+v1 = 100
+v2 = 200
+result = v1<v2 and v1!=v2
+print(result)          # True
+print("----------------")
+
+# or
+n=10
+if n%2==0 or n%3==0:
+    print("Divisible by 2 or 3")   # Divisible by 2 or 3
+print("----------------")
+
+# not
+a = 100
+b = not a>200
+print(b)               # True
+print("---------------------------------------------------------------------")
