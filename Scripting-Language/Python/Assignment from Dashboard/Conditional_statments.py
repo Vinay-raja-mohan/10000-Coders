519e74f3128cb2891ee962b78ebe2656c6e8a79d
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-01-20 23:59:49 +0530

    Python constraints assignment part -1

---
+++ Scripting-Language/Python/Assignment from Dashboard/Conditional_statments.py
@@ -0,0 +1,46 @@
+"Question 1"
+hindi = int(input())
+english = int(input())
+telugu = int(input())
+math = int(input())
+science = int(input())
+
+if hindi < 40 or english < 40 or telugu < 40 or math < 40 or science < 40:
+  print("Fail")
+
+else:
+  sum=hindi+english+telugu+math+science
+  per = (sum/5)
+  print(f"{per}% Pass")
+
+
+"Question 2"
+year = int(input())
+if(year%4==0 and year%100!=0)  or (year%400==0):
+  print("Leap Year")
+
+else:
+  print("Not Leap Year")
+
+
+"Question 3"
+sal = int(input())
+if(sal <= 250000):
+  print("No Tax")
+elif(sal>250000 and sal<=500000):
+  print(f"5% Tax {sal-sal*0.05}")
+elif(sal>500000 and sal<1000000):
+  print(f"20% Tax {sal-sal*0.2}")
+else:
+  print(f"30% Tax {sal-sal*0.3}")
+
+"Question 4"
+import math
+a = int(input())
+b= int(input())
+c = int(input())
+
+if(a+b>c and b+c>a and c+a>b):
+  s=(a+b+c)/2
+  area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+  print("area =",area)
