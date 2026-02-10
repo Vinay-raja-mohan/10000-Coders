e5a37a7450f6f98efd98db4773c365734f47770f
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-02-10 08:31:07 +0530

    Day 13 SQL Correlated Queries

---
+++ DataBases/MYSQL/Day-13/Day_13.sql
@@ -0,0 +1,49 @@
+----------nesteed query-------------------------
+--Second highest salary from the table
+select * from emp where salary =
+(select max(salary) from emp where salary <
+(select max(salary) from emp));
+
+--third higest salary
+select * from emp where salary =
+(select max(salary) from emp where salary <
+(select max(salary) from emp where salary <
+(select max(salary) from emp)));
+
+--20th max salary
+select distinct salary from emp order by salary desc limit 1 offset 19;
+
+
+-----------Correlated Query------------------------
+-- 13. Find employees earning the minimum salary in their department.
+select * from emp as e where salary =
+(select min(salary) from emp where department = e.department)
+
+-- 14. Find employees older than the average age of their department.
+select * from emp as e where age >
+(select avg(age) from emp where department = e.department);
+
+-- 15. Find employees who have the same city as at least one of their department colleagues.
+SELECT * FROM emp e WHERE EXISTS
+(SELECT 1 FROM emp WHERE department = e.department AND city = e.city AND EmpID != e.EmpID);
+
+-- 16. Find the city with the maximum number of employees.
+
+select city from emp group by city having count(*) = 
+(SELECT MAX(cnt) FROM 
+(SELECT COUNT(*) AS cnt FROM emp GROUP BY city) y);
+
+
+
+
+
+-------Views--------
+-- Creates a virtual table for the main table, 
+-- So that we can easily access the table
+-- Save the time
+-- Provide security by giving names
+
+-- create view view_name as[
+-- command ]
+
+-- select * from view_name
