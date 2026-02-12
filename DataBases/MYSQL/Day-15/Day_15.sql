f724521a75c5a8030a66169851b2b7d8d2e280ea
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-02-12 23:41:28 +0530

    day 15 Sql procedures

--- DataBases/MYSQL/Day-15/Day_15.sql
+++ DataBases/MYSQL/Day-15/Day_15.sql
@@ -1,24 +1,24 @@
---Procedures : reuse a block of code again and again
-            -- just like a function
+Procedures : reuse a block of code again and again
+            just like a function
 
--- delimiter $$
--- create Procedure procedure_name()
--- begin
--- commands
--- end$$
--- delimiter;
+delimiter $$
+create Procedure procedure_name()
+begin
+commands
+end$$
+delimiter;
 
--- calling the procedure:
--- call procedure_name();
--- exec procedure_name() -- in sql server
+calling the procedure:
+call procedure_name();
+exec procedure_name() -- in sql server
 
--- delimiter $$
--- create PROCEDURE age()
--- begin
--- select * from emp where age > 35;
--- end$$
--- delimiter;
+delimiter $$
+create PROCEDURE age()
+begin
+select * from emp where age > 35;
+end$$
+delimiter;
 
 
--- remove the proccedure
--- drop procedure procedure_name;
+remove the proccedure
+drop procedure procedure_name;
