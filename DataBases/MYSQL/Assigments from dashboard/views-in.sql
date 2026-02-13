3d3adc1300b49cc47bfbcaf9341797a802201caa
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-02-13 17:42:26 +0530

    views in sql assignment

---
+++ DataBases/MYSQL/Assigments from dashboard/views-in.sql
@@ -0,0 +1,18 @@
+create view new_view as
+select * from emp where department = 'it';
+
+select * from new_view where age>30 order by age desc;
+
+create view hr as
+select * from emp where department = 'hr';
+
+select * from hr;
+
+create view salary as
+select * from emp where salary > 70000;
+
+select * from salary order by salary asc;
+
+insert into emp values (31,'Vinay','HR',80000,33,'Hyderabad');
+
+select * from hr;
